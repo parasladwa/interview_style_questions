802392a2b5cfa2dc572ff9e95ba39585345e6e96
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-04 13:40:07 +0000

    learning cpp syntax
    through hackerrank tasks

---
+++ cpp.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+
+
+int simpleArraySum(const std::vector<int>& ar) {
+    int total = 0;
+    for (int i : ar) {
+        total += i;
+        std::cout << total ;
+    }
+
+    return 0;
+}
+/* 
+int main(){ 
+    std::vector<int> ar = {1, 2, 3, 4, 10, 11};
+    simpleArraySum(ar);
+}
+*/
+
+
+std::vector<int> compareTriplets(const std::vector<int>& a, const std::vector<int>& b) {
+
+    std::vector<int> net = {0, 0};
+
+    for (int i =0; i <= a.size(); i++){
+        
+        if (b[i] > a[i]){
+
+            net[1]++;
+
+        } else if (b[i] < a[i]) {
+
+            net[0]++;
+
+        } else {
+
+        }
+
+        }
+    
+    return net;
+}
+
+int main(){ 
+    std::vector<int> a = {5, 6, 7};
+    std::vector<int> b = {3, 6, 10};
+    std::vector<int> net = compareTriplets(a, b);
+
+    std::cout << net[0] <<  "  "  <<  net[1];
+}
+
+
+
+
