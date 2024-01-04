4f2a63a0a98a8f013b5e05030695fadcdff16ceb
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-04 11:17:03 +0000

    Update warm_up_challanges.py

--- warm_up_challanges.py
+++ warm_up_challanges.py
@@ -207,11 +207,3 @@ def primality(n):
             return "Not prime"
         
     return "Prime"
-
-print(primality(101))
-
-
-listt = [30, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 907]
-
-for i in listt:
-    print(i, primality(i))
