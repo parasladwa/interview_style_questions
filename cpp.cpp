5b9840098e775d2a1c1410c517a3410513f56ccb
Paras Ladwa <154990057+parasladwa@users.noreply.github.com>
2024-01-04 15:53:37 +0000

    cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
-
-
+#include <algorithm>
+#include <numeric>
 
 int simpleArraySum(const std::vector<int>& ar) {
     int total = 0;
@@ -78,28 +78,116 @@ long aVeryBigSum(std::vector<long>& ar) {
 
 
 
-int diagonalDifference(std::vector<std::vector<int>>& arr) {
-    int sum = 0;
+int diagonalDifference(std::vector<std::vector<int>> arr) {
+    int rank = 0;
+    int other = 0;
 
     for (int i = 0; i <  arr.size(); i++){
         std::vector<int> element = arr[i];
-        
+        int n = arr.size();
         for (int j=0; j<=element.size(); j++){
             if(i == j){
-                sum+= element[j];
-                std::cout << sum << " ";
+                rank+= element[j];
+            }
+
+            if (i == n-1-j){
+                other += element[j];
             }
         }
     }
-    return sum;
-
+    return abs(rank - other);
 
 
 }
 
+
+
+
+void plusMinus(std::vector<int>& arr) {
+
+    std::vector<float> totals = {0, 0, 0};
+    float n = arr.size();
+
+    for (int i : arr){
+        
+        if (i < 0){
+            totals[1] ++;
+        } else if (i ==0 ) {
+            totals[2] ++;
+        } else {
+            totals[0]++;
+        }
+
+        }
+
+    
+    for (int j : totals){
+        std::cout << j/n << "\n";
+    }
+
+}
+
+// int main(){
+//     std::vector<int> arr = {0, 1, -1};
+//     plusMinus(arr);
+// }
+
+
+void staircase(int& n) {
+    char space = ' ';
+    char tag = '#';
+    for (int i=1; i<=n; i++){
+        std::cout<< std::string(n-i, space);
+        std::cout<< std::string(i, tag) << std::endl;
+    }
+
+}
+
+// int main(){
+//     int val = 6;
+//     staircase(val);
+//     return 0;
+// }
+
+
+
+
+
+void miniMaxSum(std::vector<int>& arr) {
+    long max = *std::max_element(arr.begin(), arr.end());
+    long min = *min_element(arr.begin(), arr.end());
+    long long sum = std::accumulate(arr.begin(), arr.end(), 0);
+    long out1 = sum-max;
+    long out2 = sum-min;
+    std::cout<< out1 << " " << out2;
+}
 
 
 int main(){
-    std::vector<std::vector<int>> v {{1, 0, 1}, {0, 1, 0}, {1, 0, 1}};
-    diagonalDifference(v);
+    std::vector<int> vect = {1, 2, 3, 4, 5};
+    miniMaxSum(vect);
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
